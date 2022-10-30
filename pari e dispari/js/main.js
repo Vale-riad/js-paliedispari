5af256d17147df6de5aa2d95d95fcdfc20e3ae69
Vale-riad <valentinariad91@gmail.com>
2022-10-30 18:33:57 +0100

    Aggiunge contenuto in js

---
+++ pari e dispari/js/main.js
@@ -0,0 +1,14 @@
+//.chiedo all'utente pari o dispari
+const choice = prompt("scegli pari o dispari??")
+//. chiedo all'utente di scegliere un numero tra 1 e 5
+const userNumber = Number(prompt("Inserisci un numero da 1 a 5"));
+console.log(userNumber);
+//.creo una funzione per generare un numero randomico alla cpu
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+console.log(cpuNumber);
+//.sommo il totale della cpuNumber e userNumber
+const sum = cpuNumber+userNumber;
+console.log(sum);
+
